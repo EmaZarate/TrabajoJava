3c8d35ed4c660c1e6f7941ab2f5e0c8f18e82f07
mncriver <martincacciurri@gmail.com>
2016-09-27 16:05:37 -0300

    Pusimos el fondo

--- src/UI/ABMCPersonaje.java
+++ src/UI/ABMCPersonaje.java
@@ -1,19 +1,21 @@
 package UI;
 
-import java.awt.EventQueue;
 
+import java.awt.EventQueue;
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
+
+import Imagenes.*;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JButton;
+
 public class ABMCPersonaje {
 
-	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
+	public JFrame frame;
 
 	/**
 	 * Launch the application.
@@ -24,6 +26,7 @@ public class ABMCPersonaje {
 				try {
 					ABMCPersonaje window = new ABMCPersonaje();
 					window.frame.setVisible(true);
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,7 +38,11 @@ public class ABMCPersonaje {
 	 * Create the application.
 	 */
 	public ABMCPersonaje() {
+	
 		initialize();
+		
+		
+		
 	}
 
 	/**
@@ -45,54 +52,17 @@ public class ABMCPersonaje {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 455);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		ImagenPanel imagenPanel = new ImagenPanel();
+		frame.getContentPane().add(imagenPanel);
 		
-		JLabel lblId = new JLabel("ID");
-		lblId.setBounds(38, 35, 46, 14);
-		frame.getContentPane().add(lblId);
+		JButton btnCargarPersonaje = new JButton("Cargar Personaje");
+		imagenPanel.add(btnCargarPersonaje);
 		
-		JLabel lblNombre = new JLabel("Nombre");
-		lblNombre.setBounds(38, 73, 46, 14);
-		frame.getContentPane().add(lblNombre);
-		
-		JLabel lblEnergia = new JLabel("Energia");
-		lblEnergia.setBounds(38, 111, 46, 14);
-		frame.getContentPane().add(lblEnergia);
-		
-		JLabel lblDefensa = new JLabel("Defensa");
-		lblDefensa.setBounds(38, 145, 46, 14);
-		frame.getContentPane().add(lblDefensa);
-		
-		JLabel lblAtaque = new JLabel("Ataque");
-		lblAtaque.setBounds(38, 188, 46, 14);
-		frame.getContentPane().add(lblAtaque);
-		
-		JLabel lblEvasion = new JLabel("Evasion");
-		lblEvasion.setBounds(38, 219, 46, 14);
-		frame.getContentPane().add(lblEvasion);
-		
-		JLabel lblTotalrestante = new JLabel("Total/Restante");
-		lblTotalrestante.setBounds(38, 257, 94, 14);
-		frame.getContentPane().add(lblTotalrestante);
-		
-		textField = new JTextField();
-		textField.setBounds(108, 32, 86, 20);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(146, 70, 86, 20);
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
-		
-		textField_2 = new JTextField();
-		textField_2.setBounds(146, 108, 86, 20);
-		frame.getContentPane().add(textField_2);
-		textField_2.setColumns(10);
-		
-		textField_3 = new JTextField();
-		textField_3.setBounds(146, 142, 86, 20);
-		frame.getContentPane().add(textField_3);
-		textField_3.setColumns(10);
+		JLabel lblMarvelVsDc = new JLabel("Marvel vs DC");
+		lblMarvelVsDc.setForeground(new Color(255, 255, 255));
+		lblMarvelVsDc.setFont(new Font("Chinese Rocks Rg", Font.BOLD, 25));
+		imagenPanel.add(lblMarvelVsDc);
+			
+	
 	}
 }
