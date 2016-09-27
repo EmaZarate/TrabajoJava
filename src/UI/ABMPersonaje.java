f9546fda6d13d052b62adb5f611f469b0f1ee09b
mncriver <martincacciurri@gmail.com>
2016-09-27 17:19:41 -0300

    Metodo Crear

---
+++ src/UI/ABMPersonaje.java
@@ -0,0 +1,207 @@
+package UI;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import com.sun.glass.events.MouseEvent;
+
+import Entidades.Personaje;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.JTextField;
+
+public class ABMPersonaje extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtID;
+	private JTextField txtVida;
+	private JTextField txtNombre;
+	private JTextField txtAtaque;
+	private JTextField txtEnergia;
+	private JTextField txtEvasion;
+	private JTextField txtTotalRestante;
+	private Component frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ABMPersonaje frame = new ABMPersonaje();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ABMPersonaje() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 403, 420);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblId = new JLabel("ID");
+		lblId.setBounds(38, 67, 46, 14);
+		contentPane.add(lblId);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setBounds(38, 102, 46, 14);
+		contentPane.add(lblNombre);
+		
+		JLabel lblVida = new JLabel("Vida");
+		lblVida.setBounds(38, 139, 46, 14);
+		contentPane.add(lblVida);
+		
+		JLabel lblAtaque = new JLabel("Ataque");
+		lblAtaque.setBounds(38, 176, 46, 14);
+		contentPane.add(lblAtaque);
+		
+		JLabel lblEnergia = new JLabel("Energia");
+		lblEnergia.setBounds(38, 213, 46, 14);
+		contentPane.add(lblEnergia);
+		
+		JLabel lblTotalrestante = new JLabel("Evasion");
+		lblTotalrestante.setBounds(38, 250, 72, 14);
+		contentPane.add(lblTotalrestante);
+		
+		JButton btnCrear = new JButton("Crear");
+		btnCrear.addMouseListener(new MouseAdapter() {
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				agregar();
+			}});
+		
+		btnCrear.setBounds(10, 347, 88, 23);
+	
+		contentPane.add(btnCrear);
+		
+		JButton button = new JButton("Buscar");
+		button.setBounds(104, 347, 81, 23);
+		contentPane.add(button);
+		
+		JButton btnModificar = new JButton("Modificar");
+		btnModificar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnModificar.setBounds(195, 347, 84, 23);
+		contentPane.add(btnModificar);
+		
+		JButton btnEliminar = new JButton("Eliminar");
+		btnEliminar.setBounds(289, 347, 83, 23);
+		contentPane.add(btnEliminar);
+		
+		JLabel lblOpciones = new JLabel("Opciones");
+		lblOpciones.setFont(new Font("Harlow Solid Italic", Font.PLAIN, 24));
+		lblOpciones.setBounds(148, 11, 119, 41);
+		contentPane.add(lblOpciones);
+		
+		txtID = new JTextField();
+		txtID.setBounds(120, 65, 72, 17);
+		contentPane.add(txtID);
+		txtID.setColumns(10);
+		
+		txtVida = new JTextField();
+		txtVida.setColumns(10);
+		txtVida.setBounds(120, 137, 72, 17);
+		contentPane.add(txtVida);
+		
+		txtNombre = new JTextField();
+		txtNombre.setColumns(10);
+		txtNombre.setBounds(120, 100, 72, 17);
+		contentPane.add(txtNombre);
+		
+		txtAtaque = new JTextField();
+		txtAtaque.setColumns(10);
+		txtAtaque.setBounds(120, 174, 72, 17);
+		contentPane.add(txtAtaque);
+		
+		txtEnergia = new JTextField();
+		txtEnergia.setColumns(10);
+		txtEnergia.setBounds(120, 211, 72, 17);
+		contentPane.add(txtEnergia);
+		
+		txtEvasion = new JTextField();
+		txtEvasion.setColumns(10);
+		txtEvasion.setBounds(120, 247, 72, 17);
+		contentPane.add(txtEvasion);
+		
+		JLabel label = new JLabel("Total/Restante");
+		label.setBounds(38, 290, 72, 14);
+		contentPane.add(label);
+		
+		txtTotalRestante = new JTextField();
+		txtTotalRestante.setColumns(10);
+		txtTotalRestante.setBounds(120, 287, 72, 17);
+		contentPane.add(txtTotalRestante);
+	}
+	
+	
+	protected void agregar() {
+		if(datosValidos()){
+			try {
+				Personaje p = MapearDeFormulario();
+				ctrl.add(p);
+				MapearAFormulario(p);
+				//limpiarCampos();
+			} catch (ApplicationException ae) {
+				notifyUser(ae.getMessage(),ae, Level.DEBUG);
+			}
+		}
+	}
+	
+	
+	public boolean datosValidos(){
+		boolean valido=true;
+		if(txtID.getText().trim().length()==0
+			|| txtNombre.getText().trim().length()==0){
+			valido=false;
+			notifyUser("Complete todos los campos");
+		}
+		if(valido && !txtID.getText().matches("[0-9]*")){
+			valido=false;
+			notifyUser("DNI inválido");
+		}
+			
+		return valido;
+	}
+	
+	
+	
+	private void notifyUser(String mensaje) {
+		JOptionPane.showMessageDialog(this.frame, mensaje);
+	}
+	
+	
+	public Personaje MapearDeFormulario(){
+		Personaje p = new Personaje();
+		if(!txtID.getText().isEmpty()) p.setId(Integer.parseInt(txtID.getText()));
+		p.setDni(Integer.parseInt(txtDni.getText()));
+		p.setApellido(txtApellido.getText());
+		p.setNombre(txtNombre.getText());
+		p.setHabilitado(chckbxHabilitado.isSelected());
+		
+		return p;
+	}
+}
---
+++ src/UI/PantallaPrincipal.java
@@ -0,0 +1,96 @@
+package UI;
+
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+
+import Imagenes.*;
+
+
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class PantallaPrincipal {
+
+	public JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PantallaPrincipal window = new PantallaPrincipal();
+					window.frame.setVisible(true);
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PantallaPrincipal() {
+	
+		initialize();
+		
+		
+		
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 269, 308);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Marvel vs DC");
+		lblNewLabel.setFont(new Font("Friday13 Bonus NFI", Font.PLAIN, 29));
+		lblNewLabel.setBounds(55, 11, 194, 63);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JButton btnCargarPersonaje = new JButton("Opciones");
+		btnCargarPersonaje.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) { 
+				ABMPersonaje ap=new ABMPersonaje();
+				ap.setVisible(true);
+				
+			}
+		});
+		btnCargarPersonaje.setBounds(65, 147, 130, 23);
+		frame.getContentPane().add(btnCargarPersonaje);
+		
+		JButton btnjugar = new JButton("\u00A1JUGAR!");
+		btnjugar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnjugar.setBounds(65, 95, 130, 23);
+		frame.getContentPane().add(btnjugar);
+		
+		JButton btnSalir = new JButton("Salir");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnSalir.setBounds(66, 196, 130, 23);
+		frame.getContentPane().add(btnSalir);
+			
+	
+	}
+}
